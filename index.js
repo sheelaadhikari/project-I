e8fad362de54aae4a8a0c35eb9de1df955bc6d39
Sheela Adhikari <adhikarisheela1000@gmail.com>
2022-01-17 10:43:16 +0545

    strings

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-alert("hello world");
-var myStr = "this is start." + " This is end .";
-console.log(myStr);
+// alert("hello world");
+// var myStr = "this is start." + " This is end .";
+// console.log(myStr);
+
+
+
+var firstName = "sheela";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+console.log(lastLetterOfFirstName);
