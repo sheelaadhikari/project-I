2d23ad5dfbbc4e314c7a3a889efea7d1cc7a796a
Sheela Adhikari <adhikarisheela1000@gmail.com>
2022-01-24 18:45:00 +0545

    quiz app

--- index.js
+++
@@ -1,9 +0,0 @@
-// alert("hello world");
-// var myStr = "this is start." + " This is end .";
-// console.log(myStr);
-
-
-
-var firstName = "sheela";
-var lastLetterOfFirstName = firstName[firstName.length - 1];
-console.log(lastLetterOfFirstName);
