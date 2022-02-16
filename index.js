9442ac43b86cd8dccd97185353246e81b468ed32
Sheela Adhikari <adhikarisheela1000@gmail.com>
2022-02-16 15:27:09 +0545

    javascript dom manipulation

--- index.js
+++ index.js
@@ -46,17 +46,52 @@
 
 
 //return if the number is 100 or sum of the number is 100 otherwise false using javascript fumction
-function checkIt(a, b) {
-    if (a + b === 100 || (a === 100 && b === 0) || (a === 0 && b === 100)) {
-        return "true";
-    } else {
-        return "error";
-    }
+// function checkIt(a, b) {
+//     if (a + b === 100 || (a === 100 && b === 0) || (a === 0 && b === 100)) {
+//         return "true";
+//     } else {
+//         return "error";
+//     }
 
-}
-console.log(checkIt(10, 0));
-console.log(checkIt(50, 50));
-console.log(checkIt(100, 40));
-console.log(checkIt(100, 0));
-console.log(checkIt(0, 0));
-console.log(checkIt(100, 0));
+// }
+// console.log(checkIt(10, 0));
+// console.log(checkIt(50, 50));
+// console.log(checkIt(100, 40));
+// console.log(checkIt(100, 0));
+// console.log(checkIt(0, 0));
+// console.log(checkIt(100, 0));
+
+
+// string template literals
+// const Books = [{
+//         title: "Java Programming",
+//         authors: ["santosh", "niraj"],
+//         rating: 3.3,
+//     },
+//     {
+//         title: "C Programming",
+//         authors: ["nita", "shree"],
+//         rating: 3.9,
+//     },
+//     {
+//         title: " C# Programming",
+//         authors: ["santosh", "niraj"],
+//         rating: 3.6,
+//     },
+// ]
+// Books.forEach(function(b) {
+//     console.log(`
+// <ul>
+// <li>title:${b.title}</li>
+// <li>title:${b.author}</li>
+// <li>title:${b.rating}</li>
+//     `)
+// })
+console.log("heelo");
+const myp = document.getElementById("myp");
+myp.innerHTML = "claa"
+const myp = document.getElementById("mydiv");
+const myp = document.getElementById("myp2");
+const myp = document.getElementById("mydiv2");
+myp.addEventListener("click", function() { mydiv.style.background = "lightblue" });
+// myp.onclick = function() { mydiv.style.background = "lightblue" };
