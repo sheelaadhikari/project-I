186f5b82a912dab78ad610a4d27a77f542377fbd
sheelaadhikari <adhikarisheela1000@gmail.com>
2022-04-04 16:54:33 +0545

    select

--- index.php
+++ index.php
@@ -13,4 +13,37 @@ try {
 }
 
 
-?>
+
+
+$sql = "SELECT *
+FROM article
+ WHERE id = " . $_GET['id'];        
+$results = mysqli_query($conn, $sql);
+if($results === false){
+echo mysqli_error($conn);
+}
+else {
+$article = mysqli_fetch_assoc($results);             
+}
+?>        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>My Blog</title>
+</head>
+<body>
+<h1>My Blog</h1>
+<?php if($article === null): ?>
+        <p> No articles found. </p>
+        <?php else: ?>
+
+        <h2> <?= $article['title']; ?></h2>
+        <p> <?= $article['content']; ?></p>
+        
+
+<?php endif; ?>
+</body>
+</html>
