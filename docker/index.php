c8b2460e875f9b71c772cf32805f3783cefb3598
sheelaadhikari <adhikarisheela1000@gmail.com>
2022-04-04 16:06:34 +0545

    php

---
+++ docker/index.php
@@ -0,0 +1,16 @@
+<?php
+$host = "mariadb";
+$user = "testuser";
+$pass = "testpassword";
+$db = "mydb";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+?>
